1aa55b871947ac28379d0d867f8ea994d16de68e
Björn Benouarets <bbenouarets@proton.me>
2025-07-14 17:32:58 +0200

    feat: Add domain handler

---
+++ server/handler/domain.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/secnex/kit/models"
+)
+
+type DomainGetResponse struct {
+	Count   int             `json:"count"`
+	Domains []models.Domain `json:"domains"`
+}
+
+type DomainNewRequest struct {
+	Name           string `json:"name"`
+	OrganizationID string `json:"organization_id"`
+}
+
+func (h *Handler) DomainGet(w http.ResponseWriter, r *http.Request) {
+	var result []models.Domain
+
+	err := h.db.DB.Find(&result).Error
+	if err != nil {
+		InternalServerError(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(DomainGetResponse{
+		Count:   len(result),
+		Domains: result,
+	})
+}
+
+func (h *Handler) DomainNew(w http.ResponseWriter, r *http.Request) {
+	var result models.Domain
+
+	err := json.NewDecoder(r.Body).Decode(&result)
+	if err != nil {
+		BadRequest(w, r, "Invalid request body")
+		return
+	}
+
+	err = h.db.DB.Create(&result).Error
+	if err != nil {
+		InternalServerError(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
