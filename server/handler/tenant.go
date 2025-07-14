2c242ae89f21009818420b8ec2aadc7815a7ff69
Björn Benouarets <bbenouarets@proton.me>
2025-07-14 17:33:21 +0200

    feat: Add tenant handler

---
+++ server/handler/tenant.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/secnex/kit/models"
+)
+
+type TenantGetResponse struct {
+	Count   int             `json:"count"`
+	Tenants []models.Tenant `json:"tenants"`
+}
+
+type TenantNewRequest struct {
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	OrganizationID string `json:"organization_id"`
+	DomainID       string `json:"domain_id"`
+}
+
+func (h *Handler) TenantGet(w http.ResponseWriter, r *http.Request) {
+	var result []models.Tenant
+
+	err := h.db.DB.Find(&result).Error
+	if err != nil {
+		InternalServerError(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(TenantGetResponse{
+		Count:   len(result),
+		Tenants: result,
+	})
+}
+
+func (h *Handler) TenantNew(w http.ResponseWriter, r *http.Request) {
+	var result models.Tenant
+
+	err := json.NewDecoder(r.Body).Decode(&result)
+	if err != nil {
+		BadRequest(w, r, "Invalid request body")
+		return
+	}
+
+	err = h.db.DB.Create(&result).Error
+	if err != nil {
+		InternalServerError(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
