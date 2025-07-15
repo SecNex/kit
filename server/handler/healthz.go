7b089eeaaa106106cfc747e2d07ddc759873bd2c
Björn Benouarets <bbenouarets@proton.me>
2025-07-15 11:03:40 +0200

    feat: Improve healthcheck handler

--- server/handler/healthz.go
+++ server/handler/healthz.go
@@ -7,6 +7,7 @@ import (
 
 type HealthzResponse struct {
 	Status string `json:"status"`
+	OK     bool   `json:"ok"`
 }
 
 func (h *Handler) Healthz(w http.ResponseWriter, r *http.Request) {
@@ -17,17 +18,19 @@ func (h *Handler) Healthz(w http.ResponseWriter, r *http.Request) {
 
 	if databaseStatus != nil {
 		databaseStatusText = "Not OK"
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf(`{"status":"%s","ok":false}`, databaseStatusText)))
 		return
 	}
 
 	if acceptJSON {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(fmt.Sprintf(`{"status":"%s"}`, databaseStatusText)))
+		w.Write([]byte(fmt.Sprintf(`{"status":"%s","ok":true}`, databaseStatusText)))
 	} else {
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		w.Write([]byte("OK\n"))
 	}
 }
