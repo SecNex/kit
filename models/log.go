ccbc0a716421d9e7edf549677e4926ed9e034315
Björn Benouarets <bbenouarets@proton.me>
2025-07-15 11:01:28 +0200

    feat: Add json declaration for log model

--- models/log.go
+++ models/log.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type HTTPLog struct {
-	ID        uint          `gorm:"primaryKey"`
-	IPAddress string        `gorm:"type:varchar(255)"`
-	Method    string        `gorm:"type:varchar(255)"`
-	Path      string        `gorm:"type:varchar(255)"`
-	Proto     string        `gorm:"type:varchar(255)"`
-	Duration  time.Duration `gorm:"type:bigint"`
-	UserAgent string        `gorm:"type:varchar(255)"`
-	Status    int           `gorm:"type:int"`
-	CreatedAt time.Time     `gorm:"autoCreateTime"`
-	UpdatedAt time.Time     `gorm:"autoUpdateTime"`
+	ID        uint          `gorm:"primaryKey" json:"id"`
+	IPAddress string        `gorm:"type:varchar(255)" json:"ip_address"`
+	Method    string        `gorm:"type:varchar(255)" json:"method"`
+	Path      string        `gorm:"type:varchar(255)" json:"path"`
+	Proto     string        `gorm:"type:varchar(255)" json:"proto"`
+	Duration  time.Duration `gorm:"type:bigint" json:"duration"`
+	UserAgent string        `gorm:"type:varchar(255)" json:"user_agent"`
+	Status    int           `gorm:"type:int" json:"status"`
+	CreatedAt time.Time     `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time     `gorm:"autoUpdateTime" json:"updated_at"`
 }
