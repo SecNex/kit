ffe2698f64839465c8b55c7f5627eea5e0991189
Björn Benouarets <bbenouarets@proton.me>
2025-07-15 11:00:41 +0200

    feat: Add default values for org, tenant, domain

--- config/env.go
+++ config/env.go
@@ -8,41 +8,58 @@ import (
 )
 
 const (
-	EnvEnvironment      = "ENV"
-	EnvPort             = "PORT"
-	EnvDatabaseHost     = "DATABASE_HOST"
-	EnvDatabasePort     = "DATABASE_PORT"
-	EnvDatabaseUser     = "DATABASE_USER"
-	EnvDatabasePassword = "DATABASE_PASSWORD"
-	EnvDatabaseName     = "DATABASE_NAME"
-	EnvDatabaseSSLMode  = "DATABASE_SSL_MODE"
-	EnvJWTSecret        = "JWT_SECRET"
-	EnvHTTPLog          = "HTTP_LOG"
-	EnvHTTPLogFile      = "HTTP_LOG_FILE"
+	EnvEnvironment             = "ENV"
+	EnvPort                    = "PORT"
+	EnvDatabaseHost            = "DATABASE_HOST"
+	EnvDatabasePort            = "DATABASE_PORT"
+	EnvDatabaseUser            = "DATABASE_USER"
+	EnvDatabasePassword        = "DATABASE_PASSWORD"
+	EnvDatabaseName            = "DATABASE_NAME"
+	EnvDatabaseSSLMode         = "DATABASE_SSL_MODE"
+	EnvJWTSecret               = "JWT_SECRET"
+	EnvHTTPLog                 = "HTTP_LOG"
+	EnvHTTPLogFile             = "HTTP_LOG_FILE"
+	EnvDefaultOrganizationName = "DEFAULT_ORGANIZATION_NAME"
+	EnvDefaultTenantName       = "DEFAULT_TENANT_NAME"
+	EnvDefaultDomainName       = "DEFAULT_DOMAIN_NAME"
 )
 
-type Config struct {
-	Environment string
-	Port        int
-	Database    database.DatabaseConfig
-	JWTSecret   string
-	HTTPLog     bool
-	HTTPLogFile string
+type DefaultConfig struct {
+	OrganizationName string `json:"organization_name"`
+	TenantName       string `json:"tenant_name"`
+	DomainName       string `json:"domain_name"`
 }
 
-func NewConfig(databaseConfig database.DatabaseConfig, port string, jwtSecret string, httpLog bool, httpLogFile string) *Config {
+type Config struct {
+	Environment   string                  `json:"environment"`
+	Port          int                     `json:"port"`
+	Database      database.DatabaseConfig `json:"database"`
+	JWTSecret     string                  `json:"jwt_secret"`
+	HTTPLog       bool                    `json:"http_log"`
+	HTTPLogFile   string                  `json:"http_log_file"`
+	DefaultConfig DefaultConfig           `json:"default_config"`
+}
+
+func NewConfig(databaseConfig database.DatabaseConfig, port string, jwtSecret string, httpLog bool, httpLogFile string, defaultConfig DefaultConfig) *Config {
 	portInt, err := strconv.Atoi(port)
 	if err != nil {
 		portInt = 8080
 	}
 
+	environment := "development"
+	environmentVariable := os.Getenv(EnvEnvironment)
+	if environmentVariable != "" {
+		environment = environmentVariable
+	}
+
 	return &Config{
-		Environment: "development",
-		Port:        portInt,
-		Database:    databaseConfig,
-		JWTSecret:   jwtSecret,
-		HTTPLog:     httpLog,
-		HTTPLogFile: httpLogFile,
+		Environment:   environment,
+		Port:          portInt,
+		Database:      databaseConfig,
+		JWTSecret:     jwtSecret,
+		HTTPLog:       httpLog,
+		HTTPLogFile:   httpLogFile,
+		DefaultConfig: defaultConfig,
 	}
 }
 
@@ -60,6 +77,11 @@ func NewConfigEnv() *Config {
 		os.Getenv(EnvJWTSecret),
 		os.Getenv(EnvHTTPLog) == "true",
 		os.Getenv(EnvHTTPLogFile),
+		DefaultConfig{
+			OrganizationName: os.Getenv(EnvDefaultOrganizationName),
+			TenantName:       os.Getenv(EnvDefaultTenantName),
+			DomainName:       os.Getenv(EnvDefaultDomainName),
+		},
 	)
 }
 
